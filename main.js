a546ec57515a764e7607fb07d28c43e7b508bd4d
wb_hujiangtao <wb_hujiangtao@meituan.com>
2025-05-22 19:50:55 +0800

    需要return出Pinia

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+import * as Pinia from 'pinia'
 import App from './App'
 import pinia from './store'
 import { createSSRApp } from 'vue'
+import { useLiveStore } from '@/store/modules/live';
+import { useUserStore } from '@/store/modules/user';
+import { useTrtcStore } from '@/store/modules/trtc';
 
 // #ifndef VUE3
 // import Vue from 'vue'
@@ -18,7 +22,8 @@ export function createApp() {
   const app = createSSRApp(App)
   app.use(pinia)
   return {
-    app
+    app,
+    Pinia
   }
 }
 createApp()
